5fa4999b94360bb2a3b0d6906b8968d9d80cf18e
ThePseud0o <maps.arne@googlemail.com>
2021-12-11 20:55:29 +0100

    it works

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 let dimensionsX = 10;
 let dimensionsY = 10;
 let cellSize = 100;
-let visited = [];
+let visitedCells = [];
 let obstacles = [[3,1], [3,3], [1,2], [2,4], [7,4], [5,4], [0,4], [0,9], [9,8], [7,7]];
 let characters = [[8,0,"A"],[5,2,"B"],[2,3,"C"],[0,6,"D"],[5,9,"E"],[4,4,"F"],[2,5,"G"],[7,8,"H"],[7,6,"I"],[1,6,"J"]];
+let startX = 5;
+let startY = 5;
 
 function setup() {
   createCanvas(dimensionsX * cellSize, dimensionsY * cellSize);
@@ -16,6 +18,7 @@ function setup() {
   }
   addObstacles();
   addCharacters();
+  goto(startX, startY, 0);
 }
   
 function draw() {
@@ -38,16 +41,63 @@ function addCharacters() {
   });
 }
 
-function goto(x, y) {
-  if(invalid(x,y) || visited(x,y)) {
+function valid(x, y) {
+  return (constrain(x, 0, dimensionsX - 1) == x && constrain(y, 0, dimensionsY - 1) == y) && !obstacle(x, y);
+}
+
+function obstacle(x, y) {
+  for(let i = 0; i < obstacles.length; i++) {
+    if(obstacles[i][0] == x && obstacles[i][1] == y) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function invalid(x, y) {
+  return !valid(x, y);
+}
+
+function visited(x, y) {
+  for(let i = 0; i < visitedCells.length; i++) {
+    if(visitedCells[i][0] == x && visitedCells[i][1] == y) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function collect(x, y) {
+  for(let i = 0; i < characters.length; i++) {
+    let character = characters[i];
+    if(character[0] == x && character[1] == y) {
+      document.getElementById('solution').innerHTML = document.getElementById('solution').innerHTML + character[2]
+      console.log(character[2]);
+    }
+  }
+}
+
+function visit(x, y) {
+  if(visitedCells.length > 0) {
+    stroke("red")
+    strokeWeight(2);
+    let p1 = visitedCells[visitedCells.length - 1]
+    line(p1[0] * cellSize + cellSize / 2, p1[1] * cellSize + cellSize / 2, x * cellSize + cellSize / 2, y * cellSize + cellSize / 2);
+  }
+  visitedCells.push([x, y]);
+}
+
+function goto(x, y, i) {
+  console.log("Recursion level: " + i);
+  if(invalid(x, y) || visited(x, y)) {
     return;
-  } 
+  }
 
-  collect(x,y) 
-  markVisit(x,y) 
+  collect(x, y)
+  visit(x, y)
 
-  goto(x+1, y) //nach rechts 
-  goto(x, y+1) //nach unten 
-  goto(x-1, y) //nach links 
-  goto(x, y-1) //nach oben 
+  goto(x+1, y, i + 1) //nach rechts
+  goto(x, y+1, i + 1) //nach unten
+  goto(x-1, y, i + 1) //nach links
+  goto(x, y-1, i + 1) //nach oben
 }
